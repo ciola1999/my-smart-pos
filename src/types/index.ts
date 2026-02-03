05459cc5274a5719c8757500e00894a8b1988e8d
ciola1999 <ciolaarjunior31@gmail.com>
2026-02-04 01:06:57 +0700

    error types/index.ts

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,5 +1,5 @@
 // types/index.ts
-import { products } from '@/features/smart-pos/db/schema';
+import { products } from '@/db/schema';
 import { InferSelectModel } from 'drizzle-orm';
 
 export type Products = InferSelectModel<typeof products>;
